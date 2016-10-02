5ed5f4c17ab840d604ff93c12fc1719f37b5bafd
Ethan Yoder <EMY3@pct.edu>
2016-10-01 23:43:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php header( 'Location: /index.html' ) ; ?>
+<html>
+	<head>
+		<title>My PHP App</title>
+	</head>
+	<body>
+		<?php $string = "Hello World!"; 
+		echo "<p>$string</p>";	?>
+	</body>
+</html>
