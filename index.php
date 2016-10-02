2efa365124a70dff0c1bbc8591a00040f6af7568
Ethan Yoder <EMY3@pct.edu>
2016-10-01 23:54:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<?php 	$welcomeString = "Welcome to my PHP App";
 			$php = "PHP";
 		      	echo "$welcomeString";
-			print("Below is the info for " + $php);
+			print "Below is the info for " + $php;
 		      	phpinfo(); ?>
 	</body>
 </html>
