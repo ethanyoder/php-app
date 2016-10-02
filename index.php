7d63bc2a8669da8f14e003f8aba03a290099fd48
Ethan Yoder <EMY3@pct.edu>
2016-10-02 00:13:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,25 @@
 		<title>My PHP App</title>
 	</head>
 	<body>
-		<?php 	$welcomeString = "Welcome to my PHP App";
-			$phpVar = "PHP";
-		      	echo "$welcomeString<br>";
-			print "Below is the info for $phpVar";
-		      	phpinfo(); ?>
+		<?php
+			class Dorm {
+				function Dorm($oven, $showers, $hallName) {
+					$this->oven = $oven;
+					$this->showers = $showers;
+					$this->hallName = $hallName;
+				}
+			}
+			$stringVar = "Read this string";
+			$intVar = 5;
+			$doubleVar = 2.2;
+			$boolVar = true;
+			$dormObj = new Dorm(true, 2, "Little Pine");
+			while ($boolVar) {
+				echo "String: $stringVar";
+				echo "Double: $doubleVar";
+				echo "Integer: $intVar";
+				echo "Dorm Object: Oven - $dormObj->oven, Showers - $dormObj->showers, Hall Name - $dormObj->hallName";
+			}
+		?>
 	</body>
 </html>
